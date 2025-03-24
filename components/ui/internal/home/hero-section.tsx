9634af31749e2311f02d9e63e2fddb88e49bfc54
ahmedelsherbiny0 <ahmedelsherbiny7474@gmail.com>
2025-03-24 22:08:14 +0200

    fix: Hydration error in `Hero Section`

--- components/ui/internal/home/hero-section.tsx
+++ components/ui/internal/home/hero-section.tsx
@@ -4,10 +4,18 @@ import { useColorMode } from "../../color-mode";
 import Image from "next/image";
 import { useWindowType } from "@/hooks/use-window-type";
 import { Box } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export default function HeroSection() {
   const { colorMode } = useColorMode();
   const { isDesktop } = useWindowType();
+  const [imageSrc, setImageSrc] = useState("");
+
+  useEffect(() => {
+    setImageSrc(
+      `/Images/IEEE/ieee-logo-${colorMode === "light" ? "blue" : "white"}.svg`
+    );
+  }, [colorMode]);
 
   return (
     <Box
@@ -17,15 +25,15 @@ export default function HeroSection() {
         position: "relative",
       }}
     >
-      <Image
-        alt="IEEE-ZSB Logo"
-        src={`/Images/IEEE/ieee-logo-${
-          colorMode === "light" ? "blue" : "white"
-        }.svg`}
-        layout="responsive"
-        width={700}
-        height={450}
-      />
+      {imageSrc && (
+        <Image
+          alt="IEEE-ZSB Logo"
+          src={imageSrc}
+          layout="responsive"
+          width={700}
+          height={450}
+        />
+      )}
     </Box>
   );
 }
